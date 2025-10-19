e887a39b6e435b8a41d51e7897b9d4df14cc8abe
Paulo Coelho <paulohdscoelho@gmail.com>
2025-10-19 15:46:22 -0300

    [NEW] correção da sprint 2

--- backend/src/main/java/br/com/folhapagamento/interfaces/CalculadoraAdicionais.java
+++ backend/src/main/java/br/com/folhapagamento/interfaces/CalculadoraAdicionais.java
@@ -2,6 +2,7 @@ package br.com.folhapagamento.interfaces;
 
 import br.com.folhapagamento.model.Funcionario;
 
+//Interfaces em Java devem começar com I: ICalculadoraAdicionais
 public interface CalculadoraAdicionais {
     double calcularPericulosidade(Funcionario funcionario);
     double calcularInsalubridade(Funcionario funcionario);
